574e1ca64e15304db727a1c455f1ca7686e9c3ca
Joseph <thecaliman@gmail.com>
2021-03-25 17:27:40 -0700

    Initial Commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Joseph H Green</h1>
+        <h2>Custom Portfolio</h2>
+      </div>
+    );
+  }
+}
